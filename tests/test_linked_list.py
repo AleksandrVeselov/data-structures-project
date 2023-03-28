39ff814ab4cd893c738ffbcec49e0acf0b7f8dc5
Aleksandr Veselov <sanek_ves@mail.ru>
2023-03-28 22:12:43 +0300

    Написаны тесты, сделано ДЗ №5

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,32 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src import linked_list
+
+
+class TestLinkedList(unittest.TestCase):
+
+    def test_linked_list(self):
+        """Тест односвязного списка"""
+
+        # Создаем пустой односвязный список
+        ll = linked_list.LinkedList()
+
+        # Добавляем данные
+        ll.insert_beginning({'id': 1})
+        ll.insert_at_end({'id': 2})
+        ll.insert_at_end({'id': 3})
+        ll.insert_beginning({'id': 0})
+
+        # Проверяем правильность реализации связного списка и метод __str__
+        assert str(ll) == " {'id': 0} -> {'id': 1} -> {'id': 2} -> {'id': 3} -> None"
+
+    def test_init_node(self):
+        """Тест инициализатора класса Node"""
+        node1 = linked_list.Node({'id': 1})
+        node2 = linked_list.Node({'id': 2})
+        node1.next_node = node2
+
+        assert node1.data == {'id': 1}
+        assert node1.next_node.data == {'id': 2}
+
+
